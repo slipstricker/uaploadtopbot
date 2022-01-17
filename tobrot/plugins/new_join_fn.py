eb01095a30b454a1b8900da497e11296e5288cab
slipstricker <48421749+slipstricker@users.noreply.github.com>
2022-01-17 16:25:12 -0300

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -5,20 +5,8 @@ from tobrot import AUTH_CHANNEL
 async def start_fn(client, message):
     if message.chat.type == 'private':
         name = message.from_user.first_name
-        msg = f"Hey {name}!\n"
-        msg += "I am a leecher bot..\n"
-        msg += "\n\nThank You😊"
-        await message.reply_text(
-            msg,
-            parse_mode="html",
-            quote=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton('Canal Oficial @BoxMidia', url='https://t.me/boxmidia')
-                    ]
-                ]
-            )
-        )
+        msg = f"Ola {name}!\n"
+        msg += "Apenas meu dono pode me usar <3"
+        msg += "\n\nObrigado 😊"
     elif message.from_user.id in AUTH_CHANNEL:
-        await message.reply_text(f"Hey {message.from_user.first_name}!\nI'm alive.")
+        await message.reply_text(f"Ola {message.from_user.first_name}!")

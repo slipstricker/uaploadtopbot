ee034248ac2448f61309d85ba1273c43fe7effa3
slipstricker <48421749+slipstricker@users.noreply.github.com>
2022-01-17 16:33:39 -0300

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -8,5 +8,12 @@ async def start_fn(client, message):
         msg = f"Ola {name}!\n"
         msg += "Apenas meu dono pode me usar <3"
         msg += "\n\nObrigado 😊"
+        await message.reply_text(
+            msg,
+            parse_mode="html",
+            quote=True
+        )
     elif message.from_user.id in AUTH_CHANNEL:
         await message.reply_text(f"Ola {message.from_user.first_name}!")
+
+        
